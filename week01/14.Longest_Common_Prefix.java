8ef54df14e89e9fb6e6908948d5892c97b6b8f8d
Xiao Ai <ai.x@husky.neu.edu>
2017-04-03 16:08:03 -0700

    14. Longest Common Prefix
    Signed-off-by: Xiao Ai <ai.x@husky.neu.edu>

---
+++ week01/14.Longest_Common_Prefix.java
@@ -0,0 +1,24 @@
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+		if (strs.length == 0 || strs == null) {
+            return "";
+		}
+
+        String prefix = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+			while (j < prefix.length() && j < strs[i].length()) {
+				if (prefix.charAt(j) == strs[i].charAt(j)) {
+                    j++;
+				} else {
+                    break;
+				}
+			}
+			if (j == 0) {
+                return "";
+			}
+            prefix = prefix.substring(0, j);
+		}
+		 return prefix;
+	}
+}
