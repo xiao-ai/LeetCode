2970bb6aba5cc7e45e710269ac2f3a8db6963233
Xiao Ai <ai.x@husky.neu.edu>
2017-04-03 17:21:06 -0700

    Group_Anagrams.java
    Signed-off-by: Xiao Ai <ai.x@husky.neu.edu>

---
+++ week01/49.Group_Anagrams.java
@@ -0,0 +1,25 @@
+public class Solution {
+	public List<List<String>> groupAnagrams(String[] strs) {
+		if (strs == null || strs.length == 0) {
+            return new ArrayList<List<String>>();
+		}
+
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+		for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            
+			if (!map.containsKey(keyStr)) {
+                map.put(keyStr, new ArrayList<String>());
+			}
+            map.get(keyStr).add(s);
+		}
+
+	//	for(String key: map.keySet()) {
+    //        Collections.sort(map.get(key));
+	//	}
+        
+        return new ArrayList<List<String>>(map.values());
+	}
+}
