bb567e924b4508f7ced8e336f7fa9d4a75a1f78c
Xiao Ai <ai.x@husky.neu.edu>
2017-06-07 16:17:34 -0700

    recursive merge two sorted list

--- week01/23.Merge_k_Sorted_Lists.java
+++ week01/23.Merge_k_Sorted_Lists.java
@@ -29,27 +29,20 @@ public class Solution {
 	}
  
 	private ListNode mergeTwoLists(ListNode[] lists, ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy;
-		while (l1 != null && l2 != null) {
-			if (l1.val < l2.val) {
-                head.next = l1;
-                l1 = l1.next;       
-			} else {
-                head.next = l2;
-                l2 = l2.next;
-			}
-            head = head.next;
-		}
-        
-		if (l1 != null) {
-            head.next = l1;
-		}
-        
-		if (l2 != null) {
-            head.next = l2;
-		}
- 
-        return dummy.next;
+        if (l1 == null) {
+            return l2;
+        }
+                
+        if (l2 == null) {
+            return l1;
+        }
+                    
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoSortedLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoSortedLists(l1, l2.next);
+            return l2;
+        }
 	}
 }
